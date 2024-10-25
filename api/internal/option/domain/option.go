1d2f37dbc42ce810c24a3f394fa04b67b4deca75
CardenasTiago <cardenas.tiago.r@gmail.com>
2024-10-25 14:41:22 -0300

    Crud de option finalizado

--- api/internal/option/domain/option.go
+++ api/internal/option/domain/option.go
@@ -1,37 +1,40 @@
 package domain
 
-import(
+import (
 	v "suffgo/internal/option/domain/valueObjects"
+	sv "suffgo/internal/shared/domain/valueObjects"
 )
 
-type(
+type (
 	Option struct {
-		id *v.ID
-		value *v.Value
+		id    *sv.ID
+		value v.Value
 	}
 
 	OptionDTO struct {
-		ID uint `json:"id"`
+		ID    uint   `json:"id"`
 		Value string `json:"value"`
 	}
 
 	OptionCreateRequest struct {
-		Value string `json:"value`
+		Value string `json:"value"`
 	}
 )
 
-func NewOption(id *v.ID, value v.Value)
-	*Option{
-		return &Option{
-			id:	id,
-			value: value,
-		}
+func NewOption(
+	id *sv.ID,
+	value v.Value,
+) *Option {
+	return &Option{
+		id:    id,
+		value: value,
 	}
+}
 
-func(o *Option) ID() v.ID{
+func (o *Option) ID() sv.ID {
 	return *o.id
 }
 
-func(o *Option) Value() v.Value{
+func (o *Option) Value() v.Value {
 	return o.value
 }
