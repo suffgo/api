a9e0e91bb52aea7b499b8183755f74f360e805cf
CardenasTiago <cardenas.tiago.r@gmail.com>
2025-04-14 17:46:13 -0300

    Se corrigo el cambio de password

--- api/internal/users/infrastructure/userEchoHandler.go
+++ api/internal/users/infrastructure/userEchoHandler.go
@@ -351,18 +351,16 @@ func (u *UserEchoHandler) Restore(c echo.Context) error {
 }
 
 func (u *UserEchoHandler) ChangePassword(c echo.Context) error {
-	var req d.ChangePasswordRequest
-
+	var req struct {
+		NewPassword string `json:"new_password"`
+	}
+	id, err := GetAuthenticatedUserID(c)
+	if err != nil {
+		return c.JSON(http.StatusUnauthorized, map[string]string{"error": err.Error()})
+	}
 	if err := c.Bind(&req); err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]string{
 			"error": "Invalid request format: " + err.Error(),
-		})
-	}
-
-	email, err := v.NewEmail(req.Email)
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{
-			"error": "Invalid email format: " + err.Error(),
 		})
 	}
 
@@ -372,22 +370,12 @@ func (u *UserEchoHandler) ChangePassword(c echo.Context) error {
 			"error": "Invalid password format: " + err.Error(),
 		})
 	}
-
-	err = u.ChangePasswordUsecase.Execute(*email, *newPassword)
+	err = u.ChangePasswordUsecase.Execute(*id, *newPassword)
 	if err != nil {
-		switch {
-		case errors.Is(err, uerr.ErrUserNotFound):
-			return c.JSON(http.StatusNotFound, map[string]string{
-				"error": "User not found with email: " + req.Email,
-			})
-		default:
-			// Devolvemos el error real para debug
-			return c.JSON(http.StatusInternalServerError, map[string]string{
-				"error": "Failed to change password: " + err.Error(),
-			})
-		}
+		return c.JSON(http.StatusInternalServerError, map[string]string{
+			"error": "Failed to change password: " + err.Error(),
+		})
 	}
-
 	return c.JSON(http.StatusOK, map[string]string{
 		"success": "Password changed successfully",
 	})
