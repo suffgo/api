92668853bc4a4c32f366c24ed7bd560f56528e6b
CardenasTiago <cardenas.tiago.r@gmail.com>
2025-04-24 18:24:55 -0300

    Nuevo formato de imagen para room

--- api/internal/users/domain/valueObjects/image.go
+++ api/internal/users/domain/valueObjects/image.go
@@ -31,7 +31,7 @@ func NewImage(image string) (*Image, error) {
 		return nil, errors.New("error al procesar la imagen")
 	}
 
-	if mimeType != "image/png" && mimeType != "image/jpg" && mimeType != "image/jpeg" {
+	if mimeType != "image/png" && mimeType != "image/jpg" && mimeType != "image/jpeg" && mimeType != "image/webp" {
 		return nil, errors.New("formato de imagen no soportado")
 	}
 
