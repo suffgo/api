c4c64d3bb42c0d8d4dd4fff46b9e4c16d210d260
CardenasTiago <cardenas.tiago.r@gmail.com>
2025-04-24 18:43:47 -0300

    Verificacion de sala online

--- api/internal/settingsRoom/application/useCases/UpdateUsecase.go
+++ api/internal/settingsRoom/application/useCases/UpdateUsecase.go
@@ -29,12 +29,12 @@ func (u *UpdateSettingRoomUsecase) Execute(settingRoom *domain.SettingRoom) (*do
 		return nil, err
 	}
 
-	if room.State().CurrentState == "online" {
-		return nil, errors.New("La sala esta activa, no se puede modificar")
-	}
-
 	if !room.IsFormal().IsFormal {
 		return nil, errors.New("No se puede configurar salas informales")
+	}
+
+	if room.State().CurrentState == "online" {
+		return nil, errors.New("La sala esta activa, no se puede modificar")
 	}
 
 	if existingSettings == nil {
