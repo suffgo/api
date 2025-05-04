da969cba1bcec936581a2f7bd0fdd24f32ccb097
Ignacio Sanchez <nachoagusss1@gmail.com>
2025-05-04 00:03:14 -0300

    puesta a punto prod

--- api/cmd/database/postgres.go
+++ api/cmd/database/postgres.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"fmt"
+	"log"
 	"suffgo/cmd/config"
 	"sync"
 
@@ -22,23 +23,35 @@ var engine *xorm.Engine
 
 func NewPostgresDatabase(conf *config.Config) Database {
 	once.Do(func() {
-		dsn := fmt.Sprintf(
-			"host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=America/Argentina/Buenos_Aires",
-			conf.Db.Host,
-			conf.Db.User,
-			conf.Db.Password,
-			conf.Db.DBName,
-		)
+        // Construyes el DSN  
+        dsn := fmt.Sprintf(
+            "host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=America/Argentina/Buenos_Aires",
+            conf.Db.Host,
+            conf.Db.User,
+            conf.Db.Password,
+            conf.Db.DBName,
+        )
 
-		engine, err := xorm.NewEngine("postgres", dsn)
-		if err != nil {
-			panic("error to connect database")
-		}
+        // 🔍 Loggea las variables críticas
+        log.Printf("▶️  POSTGRES HOST    = %q", conf.Db.Host)
+        log.Printf("▶️  POSTGRES USER    = %q", conf.Db.User)
+        log.Printf("▶️  POSTGRES PASS    = %q", mask(conf.Db.Password))
+        log.Printf("▶️  POSTGRES DBNAME  = %q", conf.Db.DBName)
+        log.Printf("▶️  DSN              = %q", dsn)
 
-		dbInstance = &postgresDatabase{Db: engine}
-	})
+        engine, err := xorm.NewEngine("postgres", dsn)
+        if err != nil {
+            log.Fatalf("❌ error to connect database: %v", err)
+        }
 
-	return dbInstance
+        dbInstance = &postgresDatabase{Db: engine}
+    })
+
+    return dbInstance
+}
+
+func mask(s string) any {
+	panic("unimplemented")
 }
 
 func (p *postgresDatabase) GetDb() *xorm.Engine {
